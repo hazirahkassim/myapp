e577ae289593d11e2cf5cc438e4653ecbef70d07
zira <nzira0365@gmail.com>
2023-08-09 01:00:58 +0800

    heroimage & navbar visible

---
+++ src/components/HeroImg.js
@@ -0,0 +1,24 @@
+import "./HeroImgStyle.css";
+import React from 'react';
+import heroimg from "../assets/heroimg.jpg";
+import { Link } from "react-router-dom";
+
+const HeroImg = () => {
+  return (
+    <div className="hero">
+        <div className="mask">
+            <img className="into-img" src= {heroimg} alt="IntroImg"/>
+        </div>
+        <div className="content">
+            <p>I AM A FRONT END DEVELOPER</p>
+            <h1>React Developer</h1>
+            <div>
+                <Link to="/project" className="btn">Projects</Link>
+                <Link to="/contact" className="btn btn-light">Contact</Link>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default HeroImg
