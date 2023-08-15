051277520d7a0c585ef421d2c8e5a657918c3d64
zira <nzira0365@gmail.com>
2023-08-16 01:00:41 +0800

    dynamic component

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import Navbar from '../components/Navbar';
+import HeroImg2 from '../components/HeroImg2';
+import Footer from '../components/Footer';
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div>
+      <Navbar/>
+      <HeroImg2 heading="CONTACT." text="Lets have a chat" />
+      <Footer/>
+    </div>
   )
 }
 
