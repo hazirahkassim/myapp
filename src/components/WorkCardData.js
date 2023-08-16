39e7671f52a716e2009449077715532453e40eff
zira <nzira0365@gmail.com>
2023-08-17 00:03:41 +0800

    project card

---
+++ src/components/WorkCardData.js
@@ -0,0 +1,24 @@
+import pro1 from "../assets/pro1.jpg";
+import pro2 from "../assets/project2.jpg";
+import pro3 from "../assets/project3.jpg";
+const ProjectCardData = [
+    {
+        imgsrc: pro1,
+        title: "Project 1 Title Design",
+        text: "This is the description of the project that I have done before",
+        view: "https://www.youtube.com/watch?v=Nq4Mh_jTubA",
+    },
+    {
+        imgsrc: pro2,
+        title: "Project 2 Title Design",
+        text: "This is the description of the project that I have done before",
+        view: "https://www.youtube.com/watch?v=Nq4Mh_jTubA",
+    },
+    {
+        imgsrc: pro3,
+        title: "Project 3 Title Design",
+        text: "This is the description of the project that I have done before",
+        view: "https://www.youtube.com/watch?v=Nq4Mh_jTubA",
+    }
+];
+export default ProjectCardData;
