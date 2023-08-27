385d473b3c6599b0ed04d3e643cdde34315675f3
zira <nzira0365@gmail.com>
2023-08-28 01:56:04 +0800

    add new projects

--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -1,5 +1,5 @@
 import pro1 from "../assets/project1.png";
-import pro2 from "../assets/project2.jpg";
+import pro2 from "../assets/project2.png";
 import pro3 from "../assets/project3.jpg";
 const ProjectCardData = [
     {
@@ -11,9 +11,9 @@ const ProjectCardData = [
     },
     {
         imgsrc: pro2,
-        title: "Project 2 Title Design",
-        text: "This is the description of the project that I have done before",
-        view: "https://www.youtube.com/watch?v=Nq4Mh_jTubA",
+        title: "TailWind Practice Website",
+        text: "Learn Tailwind using Youtube tutorial as references.",
+        source: "https://github.com/hazirahkassim/finance-data-y1",
     },
     {
         imgsrc: pro3,
