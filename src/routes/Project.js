c20057171306b58a73964bf7f1ec9f18408234c0
zira <nzira0365@gmail.com>
2023-08-28 02:00:08 +0800

    remove pricing card

--- src/routes/Project.js
+++ src/routes/Project.js
@@ -11,7 +11,7 @@ const Project = () => {
       <Navbar/>
       <HeroImg2 heading="PROJECTS." text="Some of my most recent works" />
       <Work/>
-      <PricingCard/>
+      {/* <PricingCard/> */}
       <Footer/>
     </div>
   )
